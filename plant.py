ddf78767f02552079d0d22191f82719119906292
Willie Ku <willieku123@gmail.com>
2020-04-28 21:12:42 +0800

    Add files via upload

---
+++ plant.py
@@ -0,0 +1,32 @@
+import os
+from PIL import Image
+import ctypes
+import time
+
+global nframes
+nframes = 0
+
+def extractFrames(inGif, outFolder):
+    frame = Image.open(inGif)
+    while frame:
+        global nframes
+        frame.convert('RGB').save( '%s\\%s.jpg' % (outFolder, nframes ) , quality=100 )
+        nframes += 1
+        try:
+            frame.seek( nframes )
+        except EOFError:
+            break;
+    return True
+
+
+
+print("Do not kil this console")
+os.system("del tmp_files\*.jpg")
+extractFrames('tenor.gif', 'tmp_files')
+
+
+while True:
+    for i in range(nframes):
+        ctypes.windll.user32.SystemParametersInfoA(20, 0, "D:\\Desktop\\gif_as_wallpaper\\tmp_files\\"+str(i)+".jpg" , 3)
+        #print("D:\\Desktop\\gif_as_wallpaper\\output\\%d.jpg" % i)
+        time.sleep(1/30) # FPS
